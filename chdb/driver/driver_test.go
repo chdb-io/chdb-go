1c0bbbee9c0998d4f29541f404d8bdd977bf3837
nmreadelf <frank.q.kong@hotmail.com>
2023-12-24 20:43:08 +0800

    impl: rows logic

--- chdb/driver/driver_test.go
+++ chdb/driver/driver_test.go
@@ -14,19 +14,6 @@ func TestDb(t *testing.T) {
 		t.Errorf("ping db fail")
 	}
 	{
-		rows, err := db.Query("SELECT version()")
-		if err != nil {
-			t.Errorf("run Query fail, err:%s", err)
-		}
-		cols, err := rows.Columns()
-		if err != nil {
-			t.Errorf("get result columns fail, err: %s", err)
-		}
-		if len(cols) != 1 {
-			t.Errorf("select version(), result columns length should be 1")
-		}
-	}
-	{
 		rows, err := db.Query(`SELECT 1,'abc'`)
 		if err != nil {
 			t.Errorf("run Query fail, err:%s", err)
@@ -37,6 +24,23 @@ func TestDb(t *testing.T) {
 		}
 		if len(cols) != 2 {
 			t.Errorf("select version(), result columns length should be 1")
+		}
+		var (
+			bar int
+			foo string
+		)
+		defer rows.Close()
+		for rows.Next() {
+			err := rows.Scan(&bar, &foo)
+			if err != nil {
+				t.Errorf("scan fail, err: %s", err)
+			}
+			if bar != 1 {
+				t.Errorf("expected error")
+			}
+			if foo != "abc" {
+				t.Errorf("expected error")
+			}
 		}
 	}
 }
