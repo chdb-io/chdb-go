342e4b5a2e94a07b1f13a13e87700f78a436414d
Andrei Goncear <agoncear@malwarebytes.com>
2025-02-14 13:41:28 +0000

    fix tests

--- chdb/wrapper.go
+++ chdb/wrapper.go
@@ -4,52 +4,22 @@ import (
 	chdbpurego "github.com/chdb-io/chdb-go/chdb-purego"
 )
 
-// Query calls queryToBuffer with a default output format of "CSV" if not provided.
+// Query calls query_conn with a default in-memory session and default output format of "CSV" if not provided.
 func Query(queryStr string, outputFormats ...string) (result chdbpurego.ChdbResult, err error) {
 	outputFormat := "CSV" // Default value
 	if len(outputFormats) > 0 {
 		outputFormat = outputFormats[0]
 	}
-	return queryToBuffer(queryStr, outputFormat, "", "")
-}
-
-// queryToBuffer constructs the arguments for QueryStable and calls it.
-func queryToBuffer(queryStr, outputFormat, path, udfPath string) (result chdbpurego.ChdbResult, err error) {
-	argv := []string{"clickhouse", "--multiquery"}
-
-	// Handle output format
-	if outputFormat == "Debug" || outputFormat == "debug" {
-		argv = append(argv, "--verbose", "--log-level=trace", "--output-format=CSV")
-	} else {
-		argv = append(argv, "--output-format="+outputFormat)
+	tempSession, err := initConnection(":memory:")
+	if err != nil {
+		return nil, err
 	}
-
-	// Handle path
-	if path != "" {
-		argv = append(argv, "--path="+path)
-	}
-
-	// Add query string
-	argv = append(argv, "--query="+queryStr)
-
-	// Handle user-defined functions path
-	if udfPath != "" {
-		argv = append(argv, "--", "--user_scripts_path="+udfPath, "--user_defined_executable_functions_config="+udfPath+"/*.xml")
-	}
-
-	// Call QueryStable with the constructed arguments
-	return chdbpurego.RawQuery(len(argv), argv)
+	defer tempSession.Close()
+	return tempSession.Query(queryStr, outputFormat)
 }
 
 func initConnection(connStr string) (result chdbpurego.ChdbConn, err error) {
 	argv := []string{connStr}
 	// Call NewConnection with the constructed arguments
 	return chdbpurego.NewConnection(len(argv), argv)
-}
-
-func connQueryToBuffer(conn chdbpurego.ChdbConn, queryStr, outputFormat string) (result chdbpurego.ChdbResult, err error) {
-	if outputFormat == "" {
-		outputFormat = "CSV"
-	}
-	return conn.Query(queryStr, outputFormat)
 }
--- chdb/wrapper_test.go
+++ chdb/wrapper_test.go
@@ -6,11 +6,6 @@ import (
 
 func TestQueryToBuffer(t *testing.T) {
 	// Create a temporary directory
-	sess, err := NewSession()
-	if err != nil {
-		t.Fatalf("could not create session: %s", err)
-	}
-	defer sess.Close()
 
 	// Define test cases
 	testCases := []struct {
@@ -31,43 +26,7 @@ func TestQueryToBuffer(t *testing.T) {
 			expectedErrMsg: "",
 			expectedResult: "123\n",
 		},
-		// Session
-		{
-			name:         "Session Query 1",
-			queryStr:     "CREATE DATABASE IF NOT EXISTS testdb; ",
-			outputFormat: "CSV",
 
-			udfPath:        "",
-			expectedErrMsg: "",
-			expectedResult: "",
-		},
-		{
-			name:         "Session Query 1 bis",
-			queryStr:     "CREATE TABLE IF NOT EXISTS testdb.testtable (id UInt32) ENGINE = MergeTree() ORDER BY id;",
-			outputFormat: "CSV",
-
-			udfPath:        "",
-			expectedErrMsg: "",
-			expectedResult: "",
-		},
-		{
-			name:         "Session Query 2",
-			queryStr:     "USE testdb; INSERT INTO testtable VALUES (1), (2), (3);",
-			outputFormat: "CSV",
-
-			udfPath:        "",
-			expectedErrMsg: "",
-			expectedResult: "",
-		},
-		{
-			name:         "Session Query 3",
-			queryStr:     "SELECT * FROM testtable;",
-			outputFormat: "CSV",
-
-			udfPath:        "",
-			expectedErrMsg: "",
-			expectedResult: "1\n2\n3\n",
-		},
 		{
 			name:         "Error Query",
 			queryStr:     "SELECT * FROM nonexist; ",
@@ -83,7 +42,7 @@ func TestQueryToBuffer(t *testing.T) {
 		t.Run(tc.name, func(t *testing.T) {
 			// Call queryToBuffer
 
-			result, err := sess.Query(tc.queryStr, tc.outputFormat)
+			result, err := Query(tc.queryStr, tc.outputFormat)
 
 			// Verify
 			if tc.expectedErrMsg != "" {
